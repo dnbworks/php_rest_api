05ca117adba878ab01bc2d57482f07a8dd528419
dnbworks <dooyongnsaakobalderama@gmail.com>
2021-08-29 09:30:52 +0800

    read functionality

--- api/read.php
+++ api/read.php
@@ -26,6 +26,34 @@ $rowcount = $result->rowCount();
 // check if there's posts
 if($rowcount > 0){
     $post_arr = array();
-} else {
+    $post_arr['data'] = array();
 
+    while($row = $result->fetch(PDO::FETCH_ASSOC)){
+        extract($row);
+
+        $post_item = array(
+            'id' => $id,
+            'title' => $title,
+            'body' => html_entity_decode($body),
+            'author' => $author,
+            'category_id' => $category_id,
+            'category_name' => $category_name
+        );
+
+        // push to data
+        array_push($post_arr['data'], $post_item);
+    }
+
+    // turn to JSON
+
+    echo  json_encode($post_arr);
+
+} else {
+    echo json_encode(
+        array(
+            'message' => 'No posts found'
+        )
+    );
 }
+
+
